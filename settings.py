fee217c84fdffbed49205fb00d4926938a0be046
Jassim Saleh <jassim7256@gmail.com>
2021-05-12 23:49:44 +0100

    Updated settings.json and .py

--- settings.py
+++ settings.py
@@ -6,15 +6,17 @@ app_name = "NOVA HUB V{} ({})".format(version, development_stage)
 
 #Be careful when changing json location. If you type a false location auto update will stop functioning.
 api = "http://192.168.1.176"
-nova_hub_json_location = "/files/nova_hub.json"
-nova_hub_update_package_location = "/files/update.zip"
+nova_hub_json_location = "/files/nova_hub.json" #Where the json file is stored on the webserver.
+nova_hub_update_package_location = "/files/update.zip" #Where the update package is stored on the webserver.
 
 nova_universe_url = "https://www.youtube.com/c/TheGoldenPro" #Change this to novauniverse website domain.
 
 #Paths
 appdata_dir = "C:" + (os.path.join(os.environ["HOMEPATH"], "AppData\\Roaming"))
 path_to_appdata_folder = f"{appdata_dir}\\.NovaUniverse"
-path_to_mc_versions_folder = f"{appdata_dir}\\.minecraft\\versions"
+path_to_minecraft_folder = f"{appdata_dir}\\.minecraft"
+
+path_to_mc_versions_folder = f"{path_to_minecraft_folder}\\versions"
 path_to_terra_smp_folder = f"{path_to_appdata_folder}\\TerraSMP"
 path_to_mods_folder = f"{path_to_terra_smp_folder}\\mods"
 path_to_mc_launcher_exe = "C:\\Program Files (x86)\\Minecraft Launcher\\MinecraftLauncher.exe"
